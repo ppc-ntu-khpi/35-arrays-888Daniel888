3fb5c61a4196a1d61edfdc99351897929306675e
888Daniel888 <58972003+888Daniel888@users.noreply.github.com>
2021-02-04 10:51:13 +0300

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -22,8 +22,8 @@ public final class Exercise implements ExerciseSE {
     public int[] symmetricArray() {
         final int length;
         length = this.numbers.length;
-        int count = (length % 2 == 0) ? 1 : 0;
-        for (int i = length / 2; i < length; i++) {
+        int count = (length % 3 == 0) ? 1 : 0;
+        for (int i = length / 3; i < length; i++) {
             this.numbers[i] = this.numbers[length - i - 1] = count++; }
         return this.numbers;}
 }
